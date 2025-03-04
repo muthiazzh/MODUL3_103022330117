4c01c75e36e863565fb8224a5e2cf2e7f1ee4fe5
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-04 08:03:55 +0700

    menambahkan implementasi kalkulator

--- Form1.cs
+++ Form1.cs
@@ -16,10 +16,78 @@ namespace MODUL3
         {
             InitializeComponent();
         }
+        int a = 0;
+        int b;
+
+        
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 3;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 4;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 5;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 6;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 7;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 8;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 9;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            a += int.Parse(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            a += int.Parse(textBox1.Text);
+            textBox1.Text = a.ToString();
         }
     }
 }
